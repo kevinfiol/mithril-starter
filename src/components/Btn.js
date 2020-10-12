62a80120e9207dd6e07142551f072740acc78009
Kevin Fiol <me@kevinfiol.com>
2020-10-12 12:50:15 -0400

    Update Btn.js

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -3,7 +3,7 @@ import { btn } from './styles/Btn.css';
 
 const Btn = {
     view: ({ attrs, children }) =>
-        m('button', {...attrs, className: btn }, children)
+        m('button', { ...attrs, className: btn }, children)
 };
 
 export default Btn;
